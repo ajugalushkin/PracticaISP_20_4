4f7b26c581f33901020dc36b57bde26935b132a8
Alexander Galushkin <ajugalushkin@gmail.com>
2022-03-29 18:30:20 +0300

    Solve first task

--- Tasks/Program.cs
+++ Tasks/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+using System.IO;
 
 namespace Tasks
 {
     internal class Program
     {
+        const string PATH = "A:\\Users\\ajuga\\source\\repos\\PracticaISP_20_4\\Tasks\\";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string numbers = "1,2,3,4,5,6,7,8,9,10";
+
+            string numbersNameFile = PATH + "number.txt";
+            File.WriteAllText(numbersNameFile, numbers);
+
+            string inputString = File.ReadAllText(numbersNameFile);
+            string newString = String.Empty; //inputString.Replace(',', '|');
+
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                if (inputString[i] == ',')
+                {
+                    newString += '|';
+                }
+                else
+                {
+                    newString += inputString[i];
+                }
+            }
+
+            string numbersSepNameFile = PATH + "numbers_sep.txt";
+            File.WriteAllText(numbersSepNameFile, newString);
+
         }
     }
 }
