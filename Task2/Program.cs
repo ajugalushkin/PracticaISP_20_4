04bf2e93c5f849c182f02c5e05fadadba411a123
Alexander Galushkin <ajugalushkin@gmail.com>
2022-03-29 19:03:32 +0300

    Solve task2

---
+++ Task2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Task2
+{
+    internal class Program
+    {
+        const string PATH = "A:\\Users\\ajuga\\source\\repos\\PracticaISP_20_4\\Task2\\students.dat";
+        static void Main(string[] args)
+        {
+            string[] students = File.ReadAllLines(PATH);
+            List<string> outList = new List<string>();
+
+            int summ = 0;
+            for (int i = 0; i < students.Length; i++)
+            {
+                summ += getGrade(students[i]);
+            }
+
+            double average = summ / students.Length;
+
+            for (int i = 0; i < students.Length; i++)
+            {
+                if (getGrade(students[i]) > average)
+                {
+                    outList.Add(students[i]);
+                }
+            }
+
+            File.WriteAllLines(PATH, outList);
+
+            Console.WriteLine();
+        }
+        static int getGrade(string inputString)
+        {
+            string[] splitedString = inputString.Split('\t');
+            int grade = 0;
+            //if (splitedString[1] != "нб")
+            //{
+            //    grade = int.Parse(splitedString[1]);
+            //}
+
+            try
+            {
+                grade = int.Parse(splitedString[1]);
+            }
+            catch (Exception)
+            {
+                grade = 0;
+            }
+
+            return grade;
+        }
+    }
+}
